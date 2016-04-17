8dfc95b09aef61a28e713b009fea31598be28e70
JamesWClark <jwclark@rockhursths.edu>
2016-04-17 01:29:38 -0500

    blending two projects together, adding brackets config too

--- client/client.js
+++ client/client.js
@@ -1,1 +1,88 @@
-var socket = io.connect('http://localhost:3000');
+//followup: http://stackoverflow.com/questions/11484418/socket-io-broadcast-to-certain-users
+
+var user;
+var socket;
+var users = {};
+
+var socketServer = 'http://127.0.0.1:3000';
+
+function logPropertiesOfObject(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      console.log(key + " -> " + JSON.stringify(obj[key]));
+    }
+  }
+}
+
+function gogoSockets() {
+  socket = io.connect(socketServer);
+  
+  socket.on('msg', function(msg) {
+    $('#chat-log').append('<div><strong>' + msg.user.name + '</strong>: ' + msg.data + '</div>');
+  });
+  
+  socket.on('otherUserConnect', function(data) {
+    users[user.id] = data;
+    console.log(data.id);
+    console.log(data.image);
+    $('#usersonline').append('<img data-id="' + data.id + '" src="' + data.image + '">');
+  });
+  
+  socket.on('otherUserDisconnect', function(user) {
+    delete users[user.id];
+  });
+}
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  user = {
+    image : profile.getImageUrl(),
+    name : profile.getName(),
+    email : profile.getEmail(),
+    id : profile.getId()
+  };
+  console.log('sending user obj: ' + user);
+  socket.emit('user', user);
+  toggleCredential();
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    user = null;
+    toggleCredential();
+  });
+}
+
+function toggleCredential() {
+  if(user == null) {
+    $('#activate').hide();
+    $('#chat-log').html('');
+    $('#signin').show();    
+  } else {
+    $('#username').text(user.name);
+    $('#useremail').text(user.email);
+    $('#userid').text(user.id);
+    $('#userimage').html('<img src="' + user.image + '"/>');
+    $('#activate').show();
+    $('#signin').hide();
+  }
+}
+
+$(document).ready(function() {
+  gogoSockets();
+  
+  $('#btnSendMessage').click(function() {
+    var msg = $('#chat-input').val();
+    socket.emit('msg', msg);
+    $('#chat-input').val('');
+    $('#chat-input').focus();
+  });
+  
+  $('#chat-input').keyup(function(event) {
+    if(event.keyCode == 13) {
+      $('#btnSendMessage').click();
+    }
+  });
+  
+});
--- server.js
+++ server.js
@@ -7,34 +7,37 @@
 var http = require('http');
 var fs = require('fs');
 
-// http://stackoverflow.com/a/31557814/1161948
-function simpleStringify (object){
+var users = [];
+
+
+function simpleStringify(object) {
+    // http://stackoverflow.com/a/31557814/1161948
     var simpleObject = {};
-    for (var prop in object ){
-        if (!object.hasOwnProperty(prop)){
+    for (var prop in object) {
+        if (!object.hasOwnProperty(prop)) {
             continue;
         }
-        if (typeof(object[prop]) == 'object'){
+        if (typeof (object[prop]) == 'object') {
             continue;
         }
-        if (typeof(object[prop]) == 'function'){
+        if (typeof (object[prop]) == 'function') {
             continue;
         }
         simpleObject[prop] = object[prop];
     }
     return JSON.stringify(simpleObject); // returns cleaned up JSON
-};
+}
 
 function logPropertiesOfObject(obj) {
-  for (var key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      console.log(key + " -> " + obj[key]);
-      
-      if(obj[key] !== null && typeof obj[key] === 'object') {
-        console.log(simpleStringify(obj[key]));
-      }
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            console.log(key + " -> " + obj[key]);
+
+            if (obj[key] !== null && typeof obj[key] === 'object') {
+                console.log(simpleStringify(obj[key]));
+            }
+        }
     }
-  }
 }
 
 
@@ -43,22 +46,42 @@ var server = http.createServer().listen(3000, '127.0.0.1');
 var io = require('socket.io').listen(server);
 
 // SINGLE CLIENT CONNECTION
-io.sockets.on('connection', function(socket) {
-  
-  console.log('user connected');
-  logPropertiesOfObject(socket);
-  
-  var socketId = socket.id;
-  var clientIp = socket.request.connection.remoteAddress;
-  
-  console.log('socketId = ' + socketId);
-  console.log('clientIp = ' + clientIp);
-  
-  socket.on('disconnect', function() {
-    console.log('user disconnected');
-  });
-  
-  
+io.sockets.on('connection', function (socket) {
+
+    console.log('user connected');
+    logPropertiesOfObject(socket);
+
+    socket.on('disconnect', function () {
+        if (!socket.user) {
+            return;
+        }
+        if (users.indexOf(socket.user) > -1) {
+            console.log(socket.user.id + ' disconnected');
+            users.splice(users.indexOf(socket.user), 1);
+            socket.broadcast.emit('otherUserDisconnect', socket.user);
+        }
+        console.log('users: ' + users.length);
+    });
+
+    socket.on('user', function (user) {
+        console.log(user.id + ' connected');
+        users.push(user);
+        socket.user = user;
+        console.log('users : ' + users.length);
+        socket.broadcast.emit('otherUserConnect', user);
+    });
+
+    socket.on('msg', function (msg) {
+        io.sockets.emit('msg', {
+            user: socket.user,
+            data: msg
+        });
+    });
+
+    socket.emit('welcome', {
+        text: 'OH HAI! U R CONNECTED '
+    });
+
 });
 
 console.log('Server running at http://127.0.0.1:3000/');
